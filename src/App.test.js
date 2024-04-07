122c39cb0f41447fee3c33ba19812afd72e0a9f4
azolnonix <azizeolougbokiki93@gmail.com>
2024-04-07 14:04:01 +0200

    edit my app file

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,21 @@
 import '@testing-library/jest-dom';
 
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders Tic-Tac-Toe title', () => {
+test("renders Tic-Tac-Toe title", () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome to the TP/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders next player', () => {
+test("renders next player", () => {
   render(<App />);
   const linkElement = screen.getByText(/Next player/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders go to game start', () => {
+test("renders go to game start", () => {
   render(<App />);
   const linkElement = screen.getByText(/Reload game/i);
   expect(linkElement).toBeInTheDocument();
